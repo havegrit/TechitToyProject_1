4e0ef91cdb2b8fe7af301f8d966916be09264bd4
havegrit <shin_jaean@naver.com>
2023-03-02 09:59:00 +0900

    명언앱 1단계 구현 수정(trim() 메소드 추가)

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -11,7 +11,8 @@ public class App {
         System.out.println("== 명언 앱 ==");
         while(true){
             System.out.printf("명언) ");
-            String input = sc.nextLine();
+            //String::trim() 메소드를 통해 공백 문자를 제거할 수 있다.
+            String input = sc.nextLine().trim();
             if (input.equals("종료")) {
                 sc.close();
                 return;
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        new App(sc);
+        new App(sc).run();
     }
 }
