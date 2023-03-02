aaf52a8704218dac089b6136e295338d1118638b
havegrit <shin_jaean@naver.com>
2023-03-02 09:36:38 +0900

    명언앱 1단계 구현

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,4 +1,18 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("== 명언 앱 ==");
+        while(true){
+            System.out.printf("명언) ");
+            String input = sc.nextLine();
+            if (input.equals("종료")) {
+                sc.close();
+                return;
+            }
+        }
+    }
 }
