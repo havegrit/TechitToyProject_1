f27825c0c56a4329d259a4c68b1650dbac9b3714
havegrit <shin_jaean@naver.com>
2023-03-02 10:12:11 +0900

    명언앱 5단계 구현

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
@@ -10,6 +12,7 @@ public class App {
     public void run() {
         int cnt = 1;
         System.out.println("== 명언 앱 ==");
+        List<Quotes> quotesList = new ArrayList<>();
         while(true){
             System.out.printf("명언) ");
             String input = sc.nextLine().trim();
@@ -21,8 +24,43 @@ public class App {
                 String content = sc.nextLine();
                 System.out.printf("작가: ");
                 String writer = sc.nextLine();
-                System.out.println(cnt++ + "번 명언이 등록되었습니다.");
+                Quotes newQuote = new Quotes(content, writer);
+                newQuote.setId(cnt++);
+                quotesList.add(newQuote);
+                System.out.println(newQuote.getId() + "번 명언이 등록되었습니다.");
+            } else if (input.equals("목록")) {
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("----------------------");
+                for (Quotes quote : quotesList) {
+                    System.out.printf("%d / %s / %s\n", quote.getId(), quote.getWriter(), quote.getContent());
+                }
             }
         }
+    }
+}
+
+class Quotes {
+    private int id;
+    private String content;
+    private String writer;
+
+    Quotes(String content, String writer) {
+        this.content = content;
+        this.writer = writer;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public String getContent() {
+        return content;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }
